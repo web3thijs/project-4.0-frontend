0b6a09197b8bfec98831134f434b9335813ec7dd
Jolien Foets <57670042+JolienFoets@users.noreply.github.com>
2022-01-31 16:39:13 +0100

    JF-DashboardAdmin-UI

---
+++ src/app/modules/admin/components/dashboard-organization/dashboard-organization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-organization',
+  templateUrl: './dashboard-organization.component.html',
+  styleUrls: ['./dashboard-organization.component.scss']
+})
+export class DashboardOrganizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
